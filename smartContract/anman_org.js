fb7eea3f592c69df61ef1846cf3d86336625bae7
luoam <609892502@qq.com>
2018-05-02 22:28:24 +0800

    Update anman_org.js

--- smartContract/anman_org.js
+++ smartContract/anman_org.js
@@ -1,8 +1,38 @@
 "use strict";
 
+var ClosedItem = function (text) {
+    if (text){
+        var obj = JSON.parse(text);
+        this.key = obj.timestamp;
+        this.value = obj.value;
+        this.author = obj.author;
+        this.flag = obj.flag;
+    } else{
+        this.key = "";
+        this.author = "";
+        this.value = "";
+        this.flag = "0";
+    }
+}
+
+ClosedItem.prototype = {
+    toString:function () {
+        return JSON.stringify(this);
+    }
+}
+
+
+
 var AnmanContract = function () {
    LocalContractStorage.defineMapProperty(this, "arrayMap");
-   LocalContractStorage.defineMapProperty(this, "dataMap");
+   LocalContractStorage.defineMapProperty(this, "dataMap",{
+       parse:function (text) {
+           return new ClosedItem(text);
+       },
+       stringify:function (o) {
+           return o.toString();
+       }
+   });
    LocalContractStorage.defineProperty(this, "size");
 };
 
@@ -13,8 +43,26 @@ AnmanContract.prototype = {
 
     set: function (key, value) {
         var index = this.size;
+        key = key.trim();
+        value = value.trim();
+        if (key === "" || value === ""){
+            throw new Error("empty key / value");
+        }
+        if (value.length > 20 || key.length > 20){
+            throw new Error("key / value exceed limit length")
+        }
+        var from = Blockchain.transaction.from;
+        var closedItem = this.repo.get(key);
+        if (closedItem){
+            throw new Error("value has been occupied");
+        }
+        closedItem = new ClosedItem();
+        closedItem.author = from;
+        closedItem.key = key;
+        closedItem.value = value;
+        closedItem.flag = "1";
         this.arrayMap.set(index, key);
-        this.dataMap.set(key, value);
+        this.dataMap.set(key, closedItem);
         this.size +=1;
     },
 
